00115b49c2546cace47f681592f98d50035149a1
Mahir khan <mahirmahir8951@gmail.com>
2025-04-22 23:46:53 +0530

    Express JS

---
+++ Project-01/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const users = require("./DummyData.json")
+const fs = require("fs")
+
+const app = express();
+const PORT = 8000;
+
+// Middleware
+app.use(express.urlencoded({extended:false}))
+
+// Routes
+
+app.get("/users",(req,res)=>{
+    const html = `
+    <ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `
+
+    res.send(html)
+})
+
+
+app.get("/api/users", (req,res)=>{
+    return res.json(users)
+})
+
+
+// REST API
+
+app.route("/api/users/:id").get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> Number(user.id) === id)
+
+    return res.json(user)
+}).patch((req,res)=>{
+    // Edit user with id
+    return res.json({status:"Pending"})
+}).delete((req,res)=>{
+    // Delete user with id
+    return res.json({status:"Pending"})
+})
+
+
+app.post('/api/users', (req,res)=>{
+    const body = req.body;
+
+    users.push({...body,id:users.length + 1})
+    fs.writeFile("./DummyData.json",JSON.stringify(users), (err,data)=>{
+        return res.json({status:"success", id:users.length})
+    })
+})
+
+
+
+
+
+app.listen(PORT,()=> console.log(`Server Started at Port`))

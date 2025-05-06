95df54fadd4f96f4df9982dfd0fc2609857ca8c6
Mahir khan <mahirmahir8951@gmail.com>
2025-05-06 23:23:14 +0530

    Authentication

---
+++ Authentication/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const bcrypt  = require("bcrypt")
+
+app.get("/",function(req,res){
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("pololololoo", salt, function(err, hash) {
+            console.log(hash);
+            
+        });
+    });
+})
+
+app.listen(3000)

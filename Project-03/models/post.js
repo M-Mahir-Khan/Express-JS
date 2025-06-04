3c63ba2b0ccc17986844d8ece8ee39ac29e79ef8
Mohammed Mahir <mahirmahir8951@gmail.com>
2025-06-04 23:42:17 +0530

    Project-03

--- Project-03/models/post.js
+++ Project-03/models/post.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const postSchema = mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:user
+        ref:"user"
     },
     date:{
         type:Date,

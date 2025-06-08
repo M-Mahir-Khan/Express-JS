cb214871e769791d652c78d04e00f8696ecddb3d
Mohammed Mahir <mahirmahir8951@gmail.com>
2025-06-08 22:58:45 +0530

    Authentication

--- Authentication/app.js
+++ Authentication/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const bcrypt  = require("bcrypt")
 
+
+
 app.get("/",function(req,res){
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash("pololololoo", salt, function(err, hash) {

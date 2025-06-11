fff86460d2978267a03e628fe1e7300a9cbd6b23
Mohammed Mahir <mahirmahir8951@gmail.com>
2025-06-11 23:52:15 +0530

    Multer

---
+++ Multer/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const PORT = 8000;
+
+const multer = require('multer')
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+
+})
+
+const upload = multer({ storage: storage })
+
+app.get("/", (req, res) => {
+    return res.render("homepage")
+
+})
+
+app.post("/upload",  upload.single('profileImage'), (req, res)=> {
+    console.log(req.file);
+    
+    return res.redirect("/")
+})
+
+
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`))
